b6b5496b2dd34d7075dd7aafe0ccf2452b614cc3
OmarMWarraich <earthling986@gmail.com>
2023-09-05 12:12:15 +0500

    feat: Template Inheritance

--- market.py
+++ market.py
@@ -6,3 +6,12 @@ app = Flask(__name__)
 @app.route('/home')
 def home_page():
     return render_template('home.html')
+
+@app.route('/market')
+def market_page():
+    items = [
+        {'id': 1, 'name': 'Phone', 'barcode': '893212299897', 'price': 500, 'image_url': 'https://images.unsplash.com/photo-1511707171634-5f897ff02aa9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=200&q=80'},
+        {'id': 2, 'name': 'Laptop', 'barcode': '123985473165', 'price': 900, 'image_url': 'https://images.unsplash.com/photo-1511707171634-5f897ff02aa9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=200&q=80'},
+        {'id': 3, 'name': 'Keyboard', 'barcode': '231985128446', 'price': 150, 'image_url': 'https://images.unsplash.com/photo-1511707171634-5f897ff02aa9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=200&q=80'}
+    ]
+    return render_template('market.html', items=items)
